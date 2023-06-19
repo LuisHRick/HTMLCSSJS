c3d00deae3c0d503965894533f9ab573c460c3ec
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-06-19 14:15:51 -0300

    working but without col

--- js/flappy.js
+++ js/flappy.js
@@ -112,14 +112,41 @@ function Progresso() {
 }
 
 
-const barreira = new Barreiras(700, 1200, 350, 400)
-const passaro = new Passaro(700)
-const areaDoJogo = document.querySelector('[wm-flappy]')
+// const barreira = new Barreiras(700, 1200, 350, 400)
+// const passaro = new Passaro(700)
+// const areaDoJogo = document.querySelector('[wm-flappy]')
 
-areaDoJogo.appendChild(passaro.elemento)
-areaDoJogo.appendChild(new Progresso().elemento)
-barreira.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
-setInterval(() => {
-    barreira.animar()
-    passaro.animar()
-}, 10);
+// areaDoJogo.appendChild(passaro.elemento)
+// areaDoJogo.appendChild(new Progresso().elemento)
+// barreira.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+// setInterval(() => {
+//     barreira.animar()
+//     passaro.animar()
+// }, 10);
+
+function FlappyBird() {
+    let pontos = 0
+
+    const areaDoJogo = document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 250, 400,
+        () => progresso.atualizarPontos(++pontos)
+    )
+    const passaro = new Passaro(altura)
+
+    areaDoJogo.appendChild(progresso.elemento)
+    areaDoJogo.appendChild(passaro.elemento)
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+    this.start = () => {
+        const temporizador = setInterval(() => {
+            barreiras.animar()
+            passaro.animar()
+        }, 20)
+    }
+}
+
+new FlappyBird().start()
